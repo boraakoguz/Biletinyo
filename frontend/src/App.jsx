622b7cd85c1448c2ef3454214fec7dfba9ab718f
Egorto04 <ege.ertem@ug.bilkent.edu.tr>
2025-04-21 00:38:26 +0300

    [Frontend] React_Router_Dom Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,14 @@
-import { useEffect, useState } from "react";
+// App.jsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./MainPage";
 
 function App() {
-  const [applications, setApplications] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:8080/api/applications")
-      .then((res) => res.json())
-      .then(setApplications)
-      .catch(console.error);
-  }, []);
-
   return (
-    <div>
-      <h1>Student Applications</h1>
-      <ul>
-        {applications.map((app, index) => (
-          <li key={index}>
-            {app.sname} ({app.sid}) applied to {app.cname} ({app.cid})
-          </li>
-        ))}
-      </ul>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
